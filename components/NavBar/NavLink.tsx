a365e289b8bb97f028be95650a21fb8b4119cf8c
PAVITRA BEHARA <pavitraredmi@gmail.com>
2022-08-16 01:21:55 +0530

    ✅ bg changed

--- components/NavBar/NavLink.tsx
+++ components/NavBar/NavLink.tsx
@@ -9,7 +9,7 @@ type props = {
 const NavLink:React.FC<props> = ({link,name}) => {
   return (
     <Link href={link}>
-        <a className='text-lg font-medium py-1 px-2 m-1 hover:bg-black hover:bg-opacity-5  rounded-lg cursor-pointer ease-linear duration-150'>
+        <a className='text-lg font-medium py-1 px-2 m-1 hover:bg-neutral-100  rounded-lg cursor-pointer ease-linear duration-150'>
             {name}
         </a>
     </Link>
