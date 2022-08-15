322e9b7577fb432b4744811c45e514239ce5180a
PAVITRA BEHARA <pavitraredmi@gmail.com>
2022-08-16 00:20:17 +0530

    ✅ achivements section added

---
+++ pages/Achivements.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Container from '../layouts/Container'
+
+const Achivements = () => {
+  return (
+    <Container>
+        <div className='w-full h-screen bg-yellow-200'>Achivements</div>
+    </Container>
+  )
+}
+
+export default Achivements
