2045f725662fb2280e4d6fd66b0444692630214f
PAVITRA BEHARA <pavitraredmi@gmail.com>
2022-08-16 01:30:01 +0530

    ✅ dark button added

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -4,6 +4,9 @@ import Link from 'next/link';
 //Components
 import NavLink from './NavLink';
 
+//icons
+import {MoonIcon} from '@heroicons/react/outline'
+
 const NAV_DATA = [
     {
         href:'/',
@@ -22,7 +25,7 @@ const NavBar = () => {
   return (
     <div className='flex w-full dark:bg-slate-700 justify-center p-2 border-b'>
         <nav className='w-full max-w-5xl flex flex-row justify-between items-center'>
-            <div className='flex flex-row'>
+            <div className=' flex-row hidden md:flex'>
                {NAV_DATA.map((e,i)=>{
                 return(
                     <NavLink key={i} name={e.name} link={e.href}/>
@@ -30,7 +33,9 @@ const NavBar = () => {
                })}
             </div>
             <div>
-                <button>dark</button>
+                <button className='py-2 px-2 m-1 bg-neutral-100 hover:bg-neutral-200 rounded-lg ease-linear duration-150 active:scale-90 hover:scale-105'>
+                    <MoonIcon className='h-6 w-6 text-neutral-600'/>
+                </button>
             </div>
         </nav>
     </div>
