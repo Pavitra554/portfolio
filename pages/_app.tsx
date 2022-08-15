9a370042696cc5b09dd180afbd89176e67d3a98d
PAVITRA BEHARA <pavitraredmi@gmail.com>
2022-08-15 20:40:07 +0530

    ✅ Initial setup with Next.js,TailwindCss,Typescript and Redux

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+
+//Redux
+import { store } from '../store/store'
+import { Provider } from 'react-redux'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return(
+    <Provider store={store}>
+       <Component {...pageProps} />
+    </Provider>
+  )
 }
 
 export default MyApp
