5af105e1a076b3cdd00c83b820d4a820372b8d91
PAVITRA BEHARA <pavitraredmi@gmail.com>
2022-08-16 00:20:43 +0530

    ✅ Project section added

---
+++ pages/Projects.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Container from '../layouts/Container'
+
+const Projects = () => {
+  return (
+    <Container>
+      <div className='w-full h-screen bg-green-200 dark:bg-green-800'>Project</div>
+    </Container>
+  )
+}
+
+export default Projects
