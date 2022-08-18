d35a0db4ffd5d083fa794ca452f8dc9395adc18f
PAVITRA BEHARA <pavitraredmi@gmail.com>
2022-08-19 02:01:13 +0530

    ✅ Framer Motion added to the navbar ⚡

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -1,52 +1,70 @@
-import React from 'react';
-import Link from 'next/link';
+import React from "react";
+import Link from "next/link";
+
+//Framer Motion
+import { motion } from "framer-motion";
 
 //Components
-import NavLink from './NavLink';
+import NavLink from "./NavLink";
 
 //icons
-import {MoonIcon,SunIcon} from '@heroicons/react/outline'
-
+import { MoonIcon, SunIcon, MenuAlt4Icon } from "@heroicons/react/outline";
 
 //Redux
-import { useDispatch,useSelector } from 'react-redux';
-import { selectTheme, toggle } from '../../reducers/themeSlice';
+import { useDispatch, useSelector } from "react-redux";
+import { selectTheme, toggle } from "../../reducers/themeSlice";
 
 const NAV_DATA = [
-    {
-        href:'/',
-        name:'Home',
-    },
-    {
-        href:'/Projects',
-        name:'Projects',
-    },
-    {
-        href:'/Achivements',
-        name:'Achivements',
-    },
-]
+  {
+    href: "/",
+    name: "Home",
+  },
+  {
+    href: "/Projects",
+    name: "Projects",
+  },
+  {
+    href: "/Achivements",
+    name: "Achivements",
+  },
+];
 const NavBar = () => {
-    const value = useSelector(selectTheme);
-    const dispatch = useDispatch();
+  const value = useSelector(selectTheme);
+  const dispatch = useDispatch();
   return (
-    <div className='flex w-full bg-white dark:bg-neutral-800 justify-center p-2 border-b  dark:border-b-neutral-800 dark:text-neutral-400 text-neutral-900 ease-linear  duration-150'>
-        <nav className='w-full max-w-5xl flex flex-row justify-between items-center'>
-            <div className=' flex-row hidden md:flex'>
-               {NAV_DATA.map((e,i)=>{
-                return(
-                    <NavLink key={i} name={e.name} link={e.href}/>
-                )
-               })}
-            </div>
-            <div>
-                <button onClick={()=>dispatch(toggle())} className='py-2 px-2 m-1 bg-neutral-100 dark:bg-neutral-700 dark:hover:bg-neutral-600 hover:bg-neutral-200 rounded-lg ease-linear duration-150 active:scale-90 hover:scale-105 text-neutral-600 dark:text-neutral-400 hover:text-neutral-800 dark:hover:text-neutral-200'>
-                    {value?<SunIcon className='h-6 w-6 '/>:<MoonIcon className='h-6 w-6'/>}
-                </button>
-            </div>
-        </nav>
-    </div>
-  )
-}
+    <motion.div
+      initial={{ y: -100 }}
+      animate={{ y: 0 }}
+      transition={{ type: "spring", stiffness: 120 }}
+      className='flex w-full bg-white dark:bg-neutral-800 justify-center p-2 dark:text-neutral-400 text-neutral-900 ease-linear  duration-150 shadow-xl sticky top-0'
+    >
+      <nav className='w-full max-w-5xl flex flex-row justify-between items-center'>
+        <div className='flex md:hidden'>
+          <MenuAlt4Icon className='h-6 w-6 ' />
+        </div>
+        <div className=' flex-row hidden md:flex'>
+          {NAV_DATA.map((e, i) => {
+            return <NavLink key={i} name={e.name} link={e.href} />;
+          })}
+        </div>
+        <div>
+          <motion.button
+            whileHover={{scale:1.1}}
+            whileTap={{scale:0.7}}
+            transition={{duration:0.2}}
+            onClick={() => dispatch(toggle())}
+            className='py-2 px-2 m-1 bg-neutral-100 dark:bg-neutral-700 dark:hover:bg-neutral-600 hover:bg-neutral-200 rounded-lg text-neutral-600 dark:text-neutral-400 hover:text-neutral-800 dark:hover:text-neutral-200 ease-linear duration-100'
+          >
+            {value ? (
+              <SunIcon className='h-6 w-6 ' />
+            ) : (
+              <MoonIcon className='h-6 w-6' />
+            )}
+          </motion.button>
+        </div>
+      </nav>
+    </motion.div>
+  );
+};
 
 export default NavBar;
--- components/NavBar/NavLink.tsx
+++ components/NavBar/NavLink.tsx
@@ -1,6 +1,9 @@
 import Link from 'next/link';
 import React from 'react';
 
+//Framer Motion
+import { motion } from "framer-motion";
+
 type props = {
     name:string,
     link:string
@@ -9,9 +12,13 @@ type props = {
 const NavLink:React.FC<props> = ({link,name}) => {
   return (
     <Link href={link}>
-        <a className='text-lg font-medium py-1 px-2 m-1 hover:bg-neutral-100 dark:hover:bg-neutral-700 text-neutral-600 dark:text-neutral-400 dark:hover:text-neutral-200 hover:text-neutral-800 active:scale-90  rounded-lg cursor-pointer ease-linear duration-150'>
+        <motion.a 
+          whileHover={{scale:1.1}}
+          whileTap={{scale:0.9}}
+          transition={{duration:0.3}}
+          className='text-lg font-medium py-1 px-2 m-1 hover:bg-neutral-100 dark:hover:bg-neutral-700 text-neutral-600 dark:text-neutral-400 dark:hover:text-neutral-200 hover:text-neutral-800   rounded-lg cursor-pointer ease-linear duration-100'>
             {name}
-        </a>
+        </motion.a>
     </Link>
   )
 }
