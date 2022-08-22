185e84112a37764565d682f4a889756b5addd48d
PAVITRA BEHARA <pavitraredmi@gmail.com>
2022-08-23 00:07:35 +0530

    ✅ nav link data added

---
+++ data/navlinks.ts
@@ -0,0 +1,19 @@
+interface navdataType{
+  href:string,
+  name:string
+}
+
+export const NAV_DATA:navdataType[]= [
+    {
+      href: "/",
+      name: "Home",
+    },
+    {
+      href: "/Projects",
+      name: "Projects",
+    },
+    {
+      href: "/Achivements",
+      name: "Achivements",
+    },
+];
