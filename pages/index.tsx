f2ef56bc157e2d31f4ea31467fe71ce452c4735f
PAVITRA BEHARA <pavitraredmi@gmail.com>
2022-08-30 00:19:16 +0530

    ✅ Hero section created

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,17 @@
 import type { NextPage } from 'next'
+import Hero from '../components/Hero/Hero'
+import TechStack from '../components/TechStack/TechStack'
 
 import Container from '../layouts/Container'
 
 const Home: NextPage = () => {
   return (
     <Container>
-      <div className='w-full h-screen dark:bg-black bg-white flex justify-center items-center ease-linear duration-150 text-5xl dark:text-white'>Home</div>
+      <div className='w-screen flex flex-col ease-linear duration-150 text-5xl dark:text-white '>
+        <div className='w-full h-full rounded-full bg-violet-600 opacity-20 blur-3xl absolute -top-96   right-0 z-10 flex'></div>
+        <Hero />
+        {/* <TechStack/> */}
+      </div>
     </Container>
   )
 }
