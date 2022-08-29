daa03d50ca71a114100f8aac6731055516f630d9
PAVITRA BEHARA <pavitraredmi@gmail.com>
2022-08-30 00:26:58 +0530

    ✅ blur color added in container component

--- layouts/Container.tsx
+++ layouts/Container.tsx
@@ -6,6 +6,9 @@ import NavBar from '../components/NavBar/NavBar'
 import { useSelector } from 'react-redux';
 import { selectTheme } from '../reducers/themeSlice';
 
+//Framer motion
+import { motion } from 'framer-motion';
+
 const Container = ({children}:any) => {
   const value = useSelector(selectTheme);
 
@@ -16,6 +19,11 @@ const Container = ({children}:any) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className='dark:bg-black bg-white ease-linear duration-150'>
+      <motion.div 
+        initial={{opacity:0,y:50}}
+        animate={{opacity:0.2,y:0}}
+        transition={{delay:0.3}}
+        className='w-full h-full rounded-full bg-violet-600 opacity-20 blur-3xl absolute -top-96   right-0 z-10 flex'></motion.div>
         <NavBar/>
         {children}
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,6 @@ const Home: NextPage = () => {
   return (
     <Container>
       <div className='w-screen flex flex-col ease-linear duration-150 text-5xl dark:text-white '>
-        <div className='w-full h-full rounded-full bg-violet-600 opacity-20 blur-3xl absolute -top-96   right-0 z-10 flex'></div>
         <Hero />
         {/* <TechStack/> */}
       </div>
