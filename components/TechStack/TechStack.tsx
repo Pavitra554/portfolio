9a5532f52b9597849b2bfededb49b27a8acad752
PAVITRA BEHARA <pavitraredmi@gmail.com>
2022-08-30 00:19:44 +0530

    ✅ Hero section process save

---
+++ components/TechStack/TechStack.tsx
@@ -0,0 +1,12 @@
+import { motion } from 'framer-motion';
+import React from 'react'
+
+const TechStack = () => {
+  return (
+    <div className='max-w-screen-lg flex flex-col md:flex-row justify-between '>
+      hello
+    </div>
+  )
+}
+
+export default TechStack;
