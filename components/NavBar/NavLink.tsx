44772216482254a978cc3f5de248f69cb7979404
PAVITRA BEHARA <pavitraredmi@gmail.com>
2022-08-31 22:39:44 +0530

    ✅ NavLink final ui

--- components/NavBar/NavLink.tsx
+++ components/NavBar/NavLink.tsx
@@ -4,6 +4,8 @@ import React from 'react';
 //Framer Motion
 import { motion } from "framer-motion";
 
+
+//Props
 type props = {
     name:string,
     link:string
@@ -16,11 +18,15 @@ const NavLink:React.FC<props> = ({link,name}) => {
           whileHover={{scale:1.1}}
           whileTap={{scale:0.9}}
           transition={{duration:0.1}}
-          className='text-lg font-medium py-1 px-2 m-1 hover:bg-white/40 dark:hover:bg-black/30 text-neutral-700 dark:text-neutral-400 dark:hover:text-neutral-50 hover:text-neutral-800   rounded-lg cursor-pointer ease-linear duration-100'>
+          className={styles.navLink}>
             {name}
         </motion.a>
     </Link>
   )
 }
+const styles ={
+  navLink: `text-lg font-medium py-1 px-2 m-1 hover:bg-black/10  dark:hover:bg-white/10 text-neutral-700 dark:text-neutral-400 dark:hover:text-neutral-50 hover:text-neutral-800   rounded-lg cursor-pointer ease-linear duration-100`
+};
 
-export default NavLink
+
+export default NavLink;
