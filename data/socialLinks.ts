d65ad58433be06715ca2fedd5ba53ffa3edc2bfa
PAVITRA BEHARA <pavitraredmi@gmail.com>
2022-10-29 00:31:26 +0530

    ✅ Minor changes

---
+++ data/socialLinks.ts
@@ -0,0 +1,27 @@
+interface linkType {
+  link: string;
+  name: string;
+}
+
+export const SOCIAL_DATA: linkType[] = [
+  {
+    link: "https://github.com/Pavitra554",
+    name: "Github",
+  },
+  {
+    link: "https://leetcode.com/Pavitra554/",
+    name: "Leetcode",
+  },
+  {
+    link: "https://twitter.com/behara_pavitra",
+    name: "Twitter",
+  },
+  {
+    link: "https://www.linkedin.com/in/pavitra-behara/",
+    name: "LinkedIn",
+  },
+  {
+    link: "https://www.instagram.com/pavitra.ts/",
+    name: "Instagram",
+  },
+];
