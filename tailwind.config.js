ec5f06d0892eea4b44db78f92ac20c76c24f1ebd
PAVITRA BEHARA <pavitraredmi@gmail.com>
2022-10-29 00:32:13 +0530

    ✅ Initial header & font added

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        'noto':['Noto Sans', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
