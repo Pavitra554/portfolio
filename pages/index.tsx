65ce23f199b222887f6d3a526299b32fb12fca38
PAVITRA BEHARA <83527744+Pavitra554@users.noreply.github.com>
2022-10-31 06:57:24 +0530

    ✅ bug fixed

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ const Home: NextPage = () => {
   return (
     <Container>
       <div className='h-screen flex justify-center items-center'>
-        <div className='text-xl text-neutral-600'>Build Under Developement ⚠️</div>
+        <div className='text-xl text-neutral-600'>Build Under Development ⚠️</div>
       </div>
     </Container>
   )
