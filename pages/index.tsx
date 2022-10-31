0dcb7d4ca47ac13b2390f0528c2d7bafd3126fe9
PAVITRA BEHARA <pavitraredmi@gmail.com>
2022-10-31 06:16:47 +0530

    ✅ scrollbar changed globally

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,9 @@ import Container from '../layouts/Container'
 const Home: NextPage = () => {
   return (
     <Container>
+      <div className='h-screen flex justify-center items-center'>
+        <div className='text-xl text-neutral-600'>Build Under Developement ⚠️</div>
+      </div>
     </Container>
   )
 }
