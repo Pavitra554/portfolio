34a68fff068a11958878745d79d6e53b568b592f
PAVITRA BEHARA <pavitraredmi@gmail.com>
2022-12-26 01:26:43 +0530

    ✅ Initial home screen

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -1,22 +1,23 @@
-import React from 'react'
+import React from "react";
 import HighText from "./HighText";
 
 const About = () => {
   return (
-    <div className=''>
-        <div className='text-noto font-semibold px-1 py-2 text-xl md:text-2xl text-neutral-100 select-none'>
-            About
+    <div className='font-noto'>
+      <div className=' font-semibold px-1 py-2 text-xl md:text-2xl text-neutral-100 select-none'>
+        About
+      </div>
+      <div className='bg-neutral-800/40 hover:brightness-110 p-3 md:p-4 rounded-lg text-neutral-400/70  text-noto text-base  md:text-base tracking-wide  ease-linear duration-100'>
+        <div className='pb-2'>
+          Hey, I'm <HighText>Pavitra 👋</HighText>. I am a passionate <HighText>Developer 👨🏻‍💻</HighText> who loves to build awesome <HighText>Web Apps💻 / Apps📱</HighText>. I also love to participating in <HighText>Hackathons 🚀</HighText> so be sure to look out for me if you want a <HighText>Team Mate</HighText>.
+          
         </div>
-       <div className='bg-neutral-800/40 hover:brightness-125 p-3 md:p-4 rounded-lg text-neutral-400/70  text-noto text-base  md:text-lg tracking-wide  ease-linear duration-100'>
-            <div className='pb-2'>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima, perferendis <HighText>Highlighted Text ♻️💠</HighText> error itaque error ullam praesentium  totam ratione nulla omnis voluptatibus quasi similique eaque libero sint nostrum <HighText>TypeScript⚡</HighText> quas qui voluptates nam, dolorem id. Ex nulla corporis repellendus aspernatur necessitatibus, deserunt est maiores labore facere nobis veritatis odit eos ab <HighText>Pavitra ⚜️</HighText> autem doloribus.
-            </div>
-            <div>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima, perferendis error praesentium <HighText>Highlighted Text 🦄</HighText>  ullam  
-            </div>
-       </div>
+        <div>
+          I'm a 3rd year <HighText>Computer Science & Engineering</HighText> Undergraduate Student looking for <HighText>Internship Opportunities</HighText>.
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
