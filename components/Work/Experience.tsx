c51d76a666dfb82aea1da2da2eacbd5d932d3ce8
PAVITRA BEHARA <pavitraredmi@gmail.com>
2023-02-11 12:52:01 +0530

    ✅ Data mapped

--- components/Work/Experience.tsx
+++ components/Work/Experience.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { work_exp } from "../../data/work_exp";
 import HighText from "../About/HighText";
 import WorkCard from "./WorkCard";
 
@@ -6,19 +7,23 @@ const Experience = () => {
   return (
     <div className='font-noto w-full'>
       <div className=' font-semibold px-1 pb-5 text-xl md:text-2xl text-slate-100 select-none'>
-        Work Experience / Hackathons
+        Work Experience
       </div>
-      <div className="flex flex-col  space-y-4">
-        <WorkCard />
-        <WorkCard />
-        <WorkCard />
-      </div>
-      <div className=' font-semibold px-1 py-5 text-xl md:text-2xl text-slate-100 select-none'>
-        Volentring Experience
-      </div>
-      <div className="flex flex-col  space-y-4">
-        <WorkCard />
-        <WorkCard />
+      <div className="flex flex-col space-y-4  md:space-y-6">
+        {work_exp.map((e,i)=>{
+          return(
+            <WorkCard 
+              name={e.name} 
+              cer_link={e.cer_link} 
+              date={e.date} 
+              description={e.description}
+              proj_link={e.proj_link}
+              tags={e.tags}
+              points={e.points}
+              proj={e.proj}
+            />
+          )
+        })}
       </div>
     </div>
   );
