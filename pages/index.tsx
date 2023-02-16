6224da777c773ec55771cf8aeab707ffb8c68812
PAVITRA BEHARA <pavitraredmi@gmail.com>
2023-02-16 20:28:35 +0530

    ✅ Deploy

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,7 +22,7 @@ const Home: NextPage = () => {
         <Head>
         <title>Pavitra</title>
         <link rel='icon' href='/favicon.ico' />
-        <meta name="description" content="Pavitra's portfolio showcasing his development skills and projects."></meta>
+        {/* <meta name="description" content="Pavitra's portfolio showcasing his development skills and projects."></meta> */}
         <meta name="author" content="Pavitra Behara"></meta>
         <meta name="keywords" content="Pavitra,pavitra554,pavitra.ts,Development,Android Development,Web Development, portfolio,TailwindCSS,React.js,react,github, Next.js,Redux,Framer Motion, projects, skills"></meta>
         </Head>
