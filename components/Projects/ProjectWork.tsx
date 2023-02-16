e39d4ad869be6883dd05ecdfe83d0f2d6a14457a
PAVITRA BEHARA <pavitraredmi@gmail.com>
2023-02-16 18:12:28 +0530

    ✅ Project section done

--- components/Projects/ProjectWork.tsx
+++ components/Projects/ProjectWork.tsx
@@ -1,4 +1,6 @@
+import Link from "next/link";
 import React from "react";
+import { projects_data } from "../../data/projects._data";
 import ProjectCard from "./ProjectCard";
 
 const ProjectWork = () => {
@@ -7,11 +9,25 @@ const ProjectWork = () => {
       <div className=' font-semibold px-1 pb-5 text-xl md:text-2xl text-slate-100 select-none'>
         Projects
       </div>
-      <div className='flex flex-col  space-y-4'>
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
+      <div className='flex flex-col space-y-4  md:space-y-6'>
+        {projects_data.map((e, i) => {
+          return (
+            <ProjectCard
+              key={i}
+              name={e.name}
+              description={e.description}
+              tags={e.tags}
+              github={e.github}
+              demo={e.demo}
+            />
+          );
+        })}
       </div>
+      <Link href={'https://github.com/Pavitra554?tab=repositories'} target="_blank">
+        <div className="w-44 tracking-wider mx-auto mt-6 bg-[#27272a] hover:bg-[#27272a]/40 p-1.5 rounded-md flex justify-center items-center  text-neutral-100 shadow-md select-none hover:brightness-125 active:scale-95 ease-linear duration-100 cursor-pointer">
+          View More
+        </div>
+      </Link>
     </div>
   );
 };
