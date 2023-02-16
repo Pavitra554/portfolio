0eb34e03ba2e6612e4efd065a4d4d2cf8f8b4a35
PAVITRA BEHARA <pavitraredmi@gmail.com>
2023-02-16 18:12:00 +0530

    ✅ Initial project card done

--- components/Projects/ProjectCard.tsx
+++ components/Projects/ProjectCard.tsx
@@ -2,26 +2,39 @@ import React from "react";
 import HighText from "../About/HighText";
 
 import { TbLink } from "react-icons/tb";
+import Link from "next/link";
 
-const ProjectCard = () => {
+type props = {
+  name:string
+  tags:string[]
+  description:string
+  github:string
+  demo:string
+};
+
+const ProjectCard:React.FC<props> = ({name,tags,description,github,demo}) => {
   return (
     <div className='bg-[#27272a]/40 hover:brightness-110 p-3 md:p-4 rounded-lg text-neutral-400/70  font-noto text-base  md:text-base tracking-wide  ease-linear duration-100'>
       <div className='flex flex-col md:flex-row justify-between md:text-2xl text-xl ease-linear duration-150'>
-        <div className='flex flex-row space-x-1 items-center ease-linear duration-100 text-[#b0b0b9] hover:text-[#e3e3eb] cursor-pointer'>
-          <div>Project name</div>
+        <div className='flex flex-row space-x-1 items-center ease-linear duration-100 text-[#b0b0b9] '>
+          <div>{name}</div>
         </div>
       </div>
-      <div className='flex flex-row my-1'>
-        <div className='bg-[#27272a] text-[#b0b0b9] items-center px-1.5 py-1 rounded-md font-noto font-medium text-sm select-none'>
-          Next.js
-        </div>
+      <div className='flex flex-row flex-wrap '>
+        {tags.map((e, i) => {
+          return (
+            <div key={i} className='flex flex-row my-1 mr-2'>
+              <div className='bg-[#27272a] text-[#b0b0b9] items-center px-1.5 py-1 rounded-md font-noto font-medium text-xs md:text-sm select-none'>
+                {e}
+              </div>
+            </div>
+          );
+        })}
       </div>
       <div className='pt-2 text-[#7d7d85] leading-6'>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
-        officiis dolore autem magnam doloremque, debitis perferendis amet
-        explicabo quo. Velit libero nemo cumque corporis doloribus.
+        {description}
       </div>
-      <div className='pl-8 text-[#7d7d85] pt-2'>
+      {/* <div className='pl-8 text-[#7d7d85] pt-2'>
         <ul className='list-disc space-y-1'>
           <li>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias,
@@ -32,20 +45,24 @@ const ProjectCard = () => {
             harum. Tenetur, modi?
           </li>
         </ul>
-      </div>
+      </div> */}
       <div className='flex flex-row space-x-3 text-[#7d7d85] pt-4'>
-        <div className='flex flex-row space-x-1 items-center ease-linear duration-100 text-[#7d7d85] hover:text-[#e3e3eb] cursor-pointer'>
-          <div>Github</div>
-          <div>
-            <TbLink size={20} />
+        <Link href={github} target='_blank'>
+          <div className='flex flex-row space-x-1 items-center ease-linear duration-100 text-[#7d7d85] hover:text-[#e3e3eb] cursor-pointer'>
+            <div>Github</div>
+            <div>
+              <TbLink size={20} />
+            </div>
           </div>
-        </div>
-        <div className='flex flex-row space-x-1 items-center ease-linear duration-100 text-[#7d7d85] hover:text-[#e3e3eb] cursor-pointer'>
-          <div>Demo</div>
-          <div>
-            <TbLink size={20} />
+        </Link>
+        {demo !== '' && <Link href={demo} target='_blank'>
+          <div className='flex flex-row space-x-1 items-center ease-linear duration-100 text-[#7d7d85] hover:text-[#e3e3eb] cursor-pointer'>
+            <div>Demo</div>
+            <div>
+              <TbLink size={20} />
+            </div>
           </div>
-        </div>
+        </Link>}
       </div>
     </div>
   );
