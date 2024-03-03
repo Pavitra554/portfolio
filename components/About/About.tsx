5f59204e3148618d41c7364d38f0e05f5924555e
PAVITRA BEHARA <pavitraredmi@gmail.com>
2024-03-04 00:25:21 +0530

    UPDATE: about updated

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -9,11 +9,20 @@ const About = () => {
       </div>
       <div className='bg-[#27272a]/40 hover:brightness-110 p-3 md:p-4 rounded-lg text-[#7d7d85]  text-noto text-base  md:text-base tracking-wide  ease-linear duration-100'>
         <div className='pb-2'>
-          Hey, I'm <HighText>Pavitra 👋</HighText>. I am a passionate <HighText>Developer 👨🏻‍💻</HighText> who loves to build awesome <HighText>Web Apps💻 / Apps📱</HighText>. I also love to participating in <HighText>Hackathons 🚀</HighText> so be sure to look out for me if you want a <HighText>Team Mate</HighText>.
-          
+          Hey, I'm <HighText>Pavitra 👋</HighText>. I am a passionate{" "}
+          <HighText>Developer 👨🏻‍💻</HighText> who loves to build{" "}
+          <HighText>Cool Stuff 🚀</HighText>.
         </div>
-        <div>
-          I'm a 3rd year <HighText>Computer Science & Engineering</HighText> Undergraduate Student looking for <HighText>Internship Opportunities</HighText>.
+        <div className='pb-2'>
+          Currently, I'm diving deep into the world of software development as a
+          <HighText> Software Development Engineer Intern at Autodesk</HighText>
+          , where I'm honing my skills and gaining valuable industry experience.
+        </div>
+        <div className='pb-2'>
+          As a final year <HighText>Computer Science & Engineering</HighText>{" "}
+          undergraduate student, I'm eagerly seeking{" "}
+          <HighText>Full-Time Opportunities</HighText> to apply my knowledge and
+          passion in a dynamic environment.
         </div>
       </div>
     </div>
