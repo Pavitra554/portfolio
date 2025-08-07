a896fe090c461ef0e411e070e1a568b3f07ef7cc
Pavitra554 <pavitraredmi@gmail.com>
2025-08-07 23:08:15 +0530

    feat: enhancments

--- app/page.tsx
+++ app/page.tsx
@@ -7,33 +7,28 @@ import Hackathons from '@/components/sections/hackathons';
 import Header from '@/components/sections/header';
 import Me from '@/components/sections/me';
 import Work from '@/components/sections/work';
-import { ScrollArea } from '@/components/ui/scroll-area';
-import { ProgressiveBlur } from '@/components/magicui/progressive-blur';
 
 export default function Home() {
   return (
     <div className=" max-w-screen-sm w-full selection:bg-zinc-300 dark:selection:bg-zinc-700">
-      <ScrollArea className="relative h-screen overflow-hidden">
-        <BlurFade delay={0.1} inView>
-          <Header />
-        </BlurFade>
-        <BlurFade delay={0.1} inView>
-          <Me />
-        </BlurFade>
-        <BlurFade delay={0.1} inView>
-          <About />
-        </BlurFade>
-        <BlurFade delay={0.1} inView>
-          <Experience />
-        </BlurFade>
-        <BlurFade delay={0.1} inView>
-          <Work />
-        </BlurFade>
-        <BlurFade delay={0.1} inView>
-          <Hackathons />
-        </BlurFade>
-        <ProgressiveBlur position="bottom" height="12%" />
-      </ScrollArea>
+      <BlurFade delay={0.1} inView>
+        <Header />
+      </BlurFade>
+      <BlurFade delay={0.1} inView>
+        <Me />
+      </BlurFade>
+      <BlurFade delay={0.1} inView>
+        <About />
+      </BlurFade>
+      <BlurFade delay={0.1} inView>
+        <Experience />
+      </BlurFade>
+      <BlurFade delay={0.1} inView>
+        <Work />
+      </BlurFade>
+      <BlurFade delay={0.1} inView>
+        <Hackathons />
+      </BlurFade>
     </div>
   );
 }
